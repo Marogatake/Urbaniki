85c2cba95e7ecb0bfc70fb3f7791012431781ca8
Marogatake <69722304+Marogatake@users.noreply.github.com>
2021-05-01 01:23:01 +0900

    Add files via upload

---
+++ head.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja" class="no-js">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link rel="apple-touch-icon-precomposed" href="image/icons/57.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="image/icons/72.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="image/icons/114.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="image/icons/144.png" />
+  <link rel="shortcut icon" type="image/x-icon" href="image/icons/favicon.ico" />
+  <!-- <script>document.documentElement.className = document.documentElement.className + ' yes-js js_active js'</script> -->
+  <link rel="alternate" hreflang="nl-nl" href="https://www.urbaniki.com/magazine/" />
+  <link rel="alternate" hreflang="en-gb" href="https://www.urbaniki.com/en-gb/magazine/" />
+  <link rel="alternate" hreflang="da-dk" href="https://www.urbaniki.com/da-dk/magasin/" />
+  <link rel="alternate" hreflang="de-de" href="https://www.urbaniki.com/de-de/magazin/" />
+  <link rel="alternate" hreflang="en-de" href="https://www.urbaniki.com/en-de/magazine/" />
+  <link rel="alternate" hreflang="en-nl" href="https://www.urbaniki.com/en-nl/magazine/" />
+  <script src="https://use.typekit.net/pvj2pjn.js"></script>
+  <script>
+    try{Typekit.load({ async: true });}catch(e){}
+  </script>
+  <title>Magazine - Urbaniki</title>
+  <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+  <link rel="canonical" href="https://www.urbaniki.com/en-gb/magazine/" />
+  <meta property="og:locale" content="en_GB" />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="Magazine - Urbaniki" />
+  <meta property="og:url" content="https://www.urbaniki.com/en-gb/magazine/" />
+  <meta property="og:site_name" content="Urbaniki" />
+  <meta property="article:modified_time" content="2021-02-04T14:55:19+00:00" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <link rel='dns-prefetch' href='//fonts.googleapis.com' />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+  <link rel="stylesheet" type="text/css" href="css/ehsb8mfo/2lht2.css" media="all" />
+  <style id='dashicons-inline-css' type='text/css'>
+    [data-font="Dashicons"]:before {font-family: 'Dashicons' !important;content: attr(data-icon) !important;speak: none !important;font-weight: normal !important;font-variant: normal !important;text-transform: none !important;line-height: 1 !important;font-style: normal !important;-webkit-font-smoothing: antialiased !important;-moz-osx-font-smoothing: grayscale !important;}
+  </style>
+  <link rel="stylesheet" type="text/css" href="css/md8ynhpt/2lht2.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/mk6jr0ra/2lhti.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/jpznv9pa/2lht2.css" media="only screen and (max-width: 768px)" />
+  <link rel="stylesheet" type="text/css" href="css/fgxf0rft/2lht2.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/8abg4fq5/2lht2.css" media="all" />
+  <link rel='stylesheet' id='fonts-css' href='https://fonts.googleapis.com/css2?family=Montserrat%3Awght%40300%3B400%3B600%3B700%3B800&#038;display=swap&#038;ver=5.6.3' type='text/css' media='all' />
+  <link rel="stylesheet" type="text/css" href="css/lw2xr33v/2lht2.css" media="all" />
+  <script src='js/dfe7b1u7.js' type="text/javascript"></script>
+  <script src='js/23jkoyjp.js' type="text/javascript"></script>
+</head>
