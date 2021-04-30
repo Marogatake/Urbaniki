62b2e96666e0ae2acde64008bc4256af1c63eea7
Marogatake <69722304+Marogatake@users.noreply.github.com>
2021-04-30 23:55:15 +0900

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,90 @@
+<footer> <span class='review_large'><span class='fadein_slides wrapper'><div class="review_claim ster"><span>Official Urban Iki brand store</span></div>
+    <div class="review_claim link kiyoh"><span>お客様は私たちを9.2 / 10と評価しています</span><a href="https://www.kiyoh.com/reviews/1059919/Urbaniki.com" target="_blank"></a></div>
+    <div class="review_claim truck"><span>当日発送</span></div>
+    </span>
+    </span>
+    <script>
+      jQuery(document).ready(function () {
+      jQuery.fn.nextOrFirst = function(selector) {
+      var next = this.next(selector);
+      return (next.length) ? next : this.prevAll(selector).last();
+      };
+      jQuery('.fadein_slides div:eq(0)').addClass("spactive");
+      setInterval(function() {jQuery('.spactive').removeClass('spactive').nextOrFirst('div').addClass('spactive')},4000);
+      });
+    </script>
+    <div class="footer_wrapper row">
+      <div class="col col-2 logo_wrapper"> <a class="logofooter" href="https://www.urbaniki.com/en-gb/"></a>
+        <ul class='socials'>
+          <li><a href='https://www.instagram.com/urban_iki/' target='_blank' rel='noopener noreferrer' class='fa'><i class="fab fa-instagram"></i></a></li>
+          <li><a href='https://nl.pinterest.com/urban_iki/' target='_blank' rel='noopener noreferrer' class='fa'><i class="fab fa-pinterest"></i></a></li>
+          <li><a href='https://www.facebook.com/urbaniki' target='_blank' rel='noopener noreferrer' class='fa'><i class="fab fa-facebook-f"></i></a></li>
+        </ul>
+      </div>
+      <div class="col col-8 footer_nav">
+        <ul>
+          <li id="menu-item-4308" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4308"><a href="https://www.urbaniki.com/en-gb/about-us/">About us</a>
+            <ul class="sub-menu">
+              <li id="menu-item-4312" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4312"><a href="https://www.urbaniki.com/en-gb/about-urban-iki/">About Urban Iki</a></li>
+              <li id="menu-item-4314" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4314"><a href="https://www.urbaniki.com/en-gb/dealer-locator/">Dealer locator</a></li>
+              <li id="menu-item-4334" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4334"><a href="https://www.urbaniki.com/en-gb/safety-bicycle-seats/">Safety of our bicycle seats</a></li>
+              <li id="menu-item-4315" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4315"><a href="https://www.urbaniki.com/en-gb/design-and-innovation/">Design and innovation</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4327" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-has-children menu-item-4327"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/">Child bike seats</a>
+            <ul class="sub-menu">
+              <li id="menu-item-4328" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4328"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/design-your-own-bicycle-seat/">Design your own!</a></li>
+              <li id="menu-item-4329" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4329"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/front-bike-seat/">Front bike seats</a></li>
+              <li id="menu-item-4331" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4331"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/rear-bike-seats/rear-seats-carrier-mounting/">Rear seats carrier mounting</a></li>
+              <li id="menu-item-4332" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4332"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/rear-bike-seats/rear-seats-frame-mounting/">Rear seats frame mounting</a></li>
+              <li id="menu-item-4330" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4330"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/folding-bike-child-seat/">Junior bike seat</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4323" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-has-children menu-item-4323"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/">Bike seat accessories</a>
+            <ul class="sub-menu">
+              <li id="menu-item-4333" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4333"><a href="https://www.urbaniki.com/en-gb/childrens-bike-helmets/">Helmets</a></li>
+              <li id="menu-item-4325" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4325"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/front-seat-accessories/windscreens/">Windscreens</a></li>
+              <li id="menu-item-4324" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4324"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/front-seat-accessories/">For front seats</a></li>
+              <li id="menu-item-4326" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4326"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/rear-seat-accessories/">For rear seat</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4317" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4317"><a href="https://www.urbaniki.com/en-gb/service-and-support/">Service and support</a>
+            <ul class="sub-menu">
+              <li id="menu-item-4319" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4319"><a href="https://www.urbaniki.com/en-gb/service-and-support/faq/">Frequently asked questions</a></li>
+              <li id="menu-item-4321" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4321"><a href="https://www.urbaniki.com/en-gb/service-and-support/mounting-videos/">Mounting videos</a></li>
+              <li id="menu-item-4320" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4320"><a href="https://www.urbaniki.com/en-gb/service-and-support/manuals/">Manuals</a></li>
+              <li id="menu-item-4322" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4322"><a href="https://www.urbaniki.com/en-gb/terms-and-conditions/">Terms &#038; Conditions</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4335" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4335"><a href="https://www.urbaniki.com/en-gb/service-and-support/contact/">Contact our customer service</a>
+            <ul class="sub-menu">
+              <li id="menu-item-4336" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4336"><a href="tel:+310548549409">+31 (0)548 54 94 09</a></li>
+              <li id="menu-item-4337" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-4337"><a href="mailto:info@urbaniki.com">info@urbaniki.com</a></li>
+              <li id="menu-item-10588" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-10588"><a href="https://wa.me/31633025539">WhatsApp</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="col col-12 payments"> <span class="titel paymenttitel">Payment methods:</span>
+        <ul class='footer_payments'>
+          <li><img src='images/visa-1.jpg'></li>
+          <li><img src='images/mastercard-1.jpg'></li>
+          <li><img src='images/paypal-1.jpg'></li>
+        </ul>
+      </div>
+    </div>
+
+    <script type='text/javascript' src='js/ui/core.min.js' id='jquery-ui-core-js'></script>
+    <script type='text/javascript' src='js/ui/mouse.min.js' id='jquery-ui-mouse-js'></script>
+    <script type='text/javascript' src='js/ui/resizable.min.js' id='jquery-ui-resizable-js'></script>
+    <script type='text/javascript' src='js/ui/draggable.min.js' id='jquery-ui-draggable-js'></script>
+    <script type='text/javascript' src='js/ui/controlgroup.min.js' id='jquery-ui-controlgroup-js'></script>
+    <script type='text/javascript' src='js/ui/checkboxradio.min.js' id='jquery-ui-checkboxradio-js'></script>
+    <script type='text/javascript' src='js/ui/button.min.js' id='jquery-ui-button-js'></script>
+    <script type='text/javascript' src='js/ui/dialog.min.js' id='jquery-ui-dialog-js'></script>
+    <script type='text/javascript' src='js/prefixfree.min.js' id='prefixfree-js'></script>
+    <script type='text/javascript' src='js/functions.js' id='functions-js'></script>
+    <script type='text/javascript' src='js/lightbox.js' id='lightbox-js'></script>
+  </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="ja" class="no-js">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link rel="apple-touch-icon-precomposed" href="image/icons/57.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="image/icons/72.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="image/icons/114.png" />
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="image/icons/144.png" />
+  <link rel="shortcut icon" type="image/x-icon" href="image/icons/favicon.ico" />
+  <!-- <script>document.documentElement.className = document.documentElement.className + ' yes-js js_active js'</script> -->
+  <link rel="alternate" hreflang="nl-nl" href="https://www.urbaniki.com/magazine/" />
+  <link rel="alternate" hreflang="en-gb" href="https://www.urbaniki.com/en-gb/magazine/" />
+  <link rel="alternate" hreflang="da-dk" href="https://www.urbaniki.com/da-dk/magasin/" />
+  <link rel="alternate" hreflang="de-de" href="https://www.urbaniki.com/de-de/magazin/" />
+  <link rel="alternate" hreflang="en-de" href="https://www.urbaniki.com/en-de/magazine/" />
+  <link rel="alternate" hreflang="en-nl" href="https://www.urbaniki.com/en-nl/magazine/" />
+  <script src="https://use.typekit.net/pvj2pjn.js"></script>
+  <script>
+    try{Typekit.load({ async: true });}catch(e){}
+  </script>
+  <title>Magazine - Urbaniki</title>
+  <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+  <link rel="canonical" href="https://www.urbaniki.com/en-gb/magazine/" />
+  <meta property="og:locale" content="en_GB" />
+  <meta property="og:type" content="article" />
+  <meta property="og:title" content="Magazine - Urbaniki" />
+  <meta property="og:url" content="https://www.urbaniki.com/en-gb/magazine/" />
+  <meta property="og:site_name" content="Urbaniki" />
+  <meta property="article:modified_time" content="2021-02-04T14:55:19+00:00" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <link rel='dns-prefetch' href='//fonts.googleapis.com' />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+  <link rel="stylesheet" type="text/css" href="css/ehsb8mfo/2lht2.css" media="all" />
+  <style id='dashicons-inline-css' type='text/css'>
+    [data-font="Dashicons"]:before {font-family: 'Dashicons' !important;content: attr(data-icon) !important;speak: none !important;font-weight: normal !important;font-variant: normal !important;text-transform: none !important;line-height: 1 !important;font-style: normal !important;-webkit-font-smoothing: antialiased !important;-moz-osx-font-smoothing: grayscale !important;}
+  </style>
+  <link rel="stylesheet" type="text/css" href="css/md8ynhpt/2lht2.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/mk6jr0ra/2lhti.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/jpznv9pa/2lht2.css" media="only screen and (max-width: 768px)" />
+  <link rel="stylesheet" type="text/css" href="css/fgxf0rft/2lht2.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/8abg4fq5/2lht2.css" media="all" />
+  <link rel='stylesheet' id='fonts-css' href='https://fonts.googleapis.com/css2?family=Montserrat%3Awght%40300%3B400%3B600%3B700%3B800&#038;display=swap&#038;ver=5.6.3' type='text/css' media='all' />
+  <link rel="stylesheet" type="text/css" href="css/lw2xr33v/2lht2.css" media="all" />
+  <script src='js/dfe7b1u7.js' type="text/javascript"></script>
+  <script src='js/23jkoyjp.js' type="text/javascript"></script>
+</head>
+<body class="page-template-default page page-id-3433 theme-urbaniki vpc-modern-configurator woocommerce-no-js">
+  <div class="hover_div"></div>
+  <header class="header">
+    <div class="header_wrapper">
+      <div class="main_nav"> <a class="closeBar" rel="main_nav"></a>
+        <ul>
+          <li id="menu-item-12911" class="design menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-12911"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/design-your-own-bicycle-seat/">Design your<br>bicycle seat!</a></li>
+          <li id="menu-item-4279" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4279"><a href="https://www.urbaniki.com/en-gb/about-us/">About us</a>
+            <ul class="sub-menu">
+              <li id="menu-item-8205" class="hide menu-item menu-item-type-post_type menu-item-object-page menu-item-8205"><a href="https://www.urbaniki.com/en-gb/about-us/">About us</a></li>
+              <li id="menu-item-4278" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4278"><a href="https://www.urbaniki.com/en-gb/about-urban-iki/">About Urban Iki</a></li>
+              <li id="menu-item-4280" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4280"><a href="https://www.urbaniki.com/en-gb/dealer-locator/">Dealer locator</a></li>
+              <li id="menu-item-4282" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4282"><a href="https://www.urbaniki.com/en-gb/safety-bicycle-seats/">Safety</a></li>
+              <li id="menu-item-4283" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4283"><a href="https://www.urbaniki.com/en-gb/design-and-innovation/">Design and innovation</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4288" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-has-children menu-item-4288"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/">Child bike seats</a>
+            <ul class="sub-menu">
+              <li id="menu-item-8206" class="hide menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-8206"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/">Child bike seats</a></li>
+              <li id="menu-item-4284" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4284"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/design-your-own-bicycle-seat/">Design your own!</a></li>
+              <li id="menu-item-4285" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4285"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/front-bike-seat/">Front seats</a></li>
+              <li id="menu-item-4296" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4296"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/rear-bike-seats/">Rear seats</a></li>
+              <li id="menu-item-4286" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4286"><a href="https://www.urbaniki.com/en-gb/child-bike-seats/folding-bike-child-seat/">Junior seat</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4295" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4295"><a href="https://www.urbaniki.com/en-gb/childrens-bike-helmets/">Helmets</a></li>
+          <li id="menu-item-4291" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-has-children menu-item-4291"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/">Accessories</a>
+            <ul class="sub-menu">
+              <li id="menu-item-8207" class="hide menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-8207"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/">Accessories</a></li>
+              <li id="menu-item-4293" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4293"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/front-seat-accessories/windscreens/">Windscreens</a></li>
+              <li id="menu-item-4292" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4292"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/front-seat-accessories/">Front seat accessories</a></li>
+              <li id="menu-item-4294" class="menu-item menu-item-type-taxonomy menu-item-object-product_cat menu-item-4294"><a href="https://www.urbaniki.com/en-gb/bike-seat-accessories/rear-seat-accessories/">Rear seat accessories</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4298" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-4298"><a href="https://www.urbaniki.com/en-gb/service-and-support/">Service and support</a>
+            <ul class="sub-menu">
+              <li id="menu-item-8208" class="hide menu-item menu-item-type-post_type menu-item-object-page menu-item-8208"><a href="https://www.urbaniki.com/en-gb/service-and-support/">Service and support</a></li>
+              <li id="menu-item-4300" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4300"><a href="https://www.urbaniki.com/en-gb/service-and-support/faq/">Frequently asked questions</a></li>
+              <li id="menu-item-4303" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4303"><a href="https://www.urbaniki.com/en-gb/service-and-support/mounting-videos/">Mounting videos</a></li>
+              <li id="menu-item-4301" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4301"><a href="https://www.urbaniki.com/en-gb/service-and-support/manuals/">Manuals</a></li>
+              <li id="menu-item-4311" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-4311"><a href="https://www.urbaniki.com/en-gb/service-and-support/contact/">Contact</a></li>
+            </ul>
+          </li>
+          <li id="menu-item-4297" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-3433 current_page_item menu-item-4297"><a href="https://www.urbaniki.com/en-gb/magazine/" aria-current="page">Magazine</a></li>
+          <li id="menu-item-4410" class="account menu-item menu-item-type-post_type menu-item-object-page menu-item-4410"><a href="https://www.urbaniki.com/en-gb/my-account/">My Account</a></li>
+        </ul>
+      </div> <a class="logo" href="https://www.urbaniki.com/en-gb/">Urban Iki</a>
+      <div class="cart_row woocommerce">
+        <div class="toggle_top_language"><img src='images/flags/united-kingdom.svg'></div>
+        <ul class="metamenu">
+          <li id="menu-item-3548" class="search searchToggle menu-item menu-item-type-custom menu-item-object-custom menu-item-3548"><a href="#">Menu item</a></li>
+          <li id="menu-item-3549" class="wishlist menu-item menu-item-type-post_type menu-item-object-page menu-item-3549"><a href="https://www.urbaniki.com/en-gb/wishlist/">Wishlist</a></li>
+          <li id="menu-item-3550" class="account menu-item menu-item-type-post_type menu-item-object-page menu-item-3550"><a href="https://www.urbaniki.com/en-gb/my-account/">My Account</a></li>
+        </ul>
+        <div class=" widget_shopping_cart_content">
+          <p class="total item-count"><a href="#"><span>0</span></a></p>
+          <div class="minicart_panel"><a class="closeMiniCart"></a>
+            <h4>Shopping bag</h4>
+            <p class="woocommerce-mini-cart__empty-message">No products in the basket.</p>
+          </div>
+        </div>
+        <div class="toggle_menu"><i class="fas fa-bars"></i></div>
+      </div>
+    </div>
+  </header>
+  <div class="searchBar">
+    <div class="searchWrap"> <a class="closeBar" rel="searchBar"></a>
+      <h4>Search</h4>
+      <form role="search" method="get" class="searchForm" id="searchform" action="https://www.urbaniki.com/en-gb/"> <input type="text" class="searchInput" placeholder="What are you looking for?" name="s" id="s" /> <button type="submit" class="searchBtn" id="searchsubmit"><i class="fa fa-search"></i></button> </form>
+    </div>
+  </div>
+  <div class="languageBar">
+    <div class="languageWrap"> <a class="closeBar" rel="languageBar"></a>
+      <h4>あなたの場所</h4>あなたの場所はあなたの通貨とあなたが出荷できる国を決定します。
+      <ul>
+        <h5 class='toggle_language'><img src="images/flags/netherlands.svg">Nederland/België</h5>
+        <li><a href="https://www.urbaniki.com/magazine/">Nederlands (€)</a></li>
+        <li><a href="https://www.urbaniki.com/en-nl/magazine/">English (€)</a></li>
+      </ul>
+      <ul>
+        <h5 class='toggle_language'><img src="images/flags/united-kingdom.svg">United Kingdom</h5>
+        <li><a href="https://www.urbaniki.com/en-gb/magazine/">English (£)</a></li>
+      </ul>
+      <ul>
+        <h5 class='toggle_language'><img src="images/flags/germany.svg">Deutschland</h5>
+        <li><a href="https://www.urbaniki.com/de-de/magazin/">Deutsch (€)</a></li>
+        <li><a href="https://www.urbaniki.com/en-de/magazine/">English (€)</a></li>
+      </ul>
+      <ul>
+        <h5 class='toggle_language'><img src="images/flags/denmark.svg">Danmark</h5>
+        <li><a href="https://www.urbaniki.com/da-dk/magasin/">Dansk (DKK)</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="reviewBar">
+    <div class="reviewWrap"> <a class="closeBar" rel="reviewBar"></a>
+      <h4>Reviews</h4>
+    </div>
+  </div> <span class='review_large'><span class='fadein_slides wrapper'><div class="review_claim ster"><span>Official Urban Iki brand store</span></div>
+  <div class="review_claim link kiyoh"><span>お客様は私たちを9.2 / 10と評価しています</span><a href="https://www.kiyoh.com/reviews/1059919/Urbaniki.com" target="_blank"></a></div>
+  <div class="review_claim truck"><span>当日発送</span></div>
+  </span>
+  </span>
+  <script>
+    jQuery(document).ready(function () {
+    jQuery.fn.nextOrFirst = function(selector) {
+    var next = this.next(selector);
+    return (next.length) ? next : this.prevAll(selector).last();
+    };
+    jQuery('.fadein_slides div:eq(0)').addClass("spactive");
+    setInterval(function() {jQuery('.spactive').removeClass('spactive').nextOrFirst('div').addClass('spactive')},4000);
+    });
+  </script>
